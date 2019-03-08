facdf0aeef0a050f7ed6bbc9c520c583a0537b3c
cetecom <cetecom@LCV20410>
2019-03-08 15:49:17 -0300

    cambios y nuevos archivos

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include "programacion.php";
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -10,10 +14,9 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        
-        
-        
+         
         <?php
+        require resultado.php;
         $A=10;
         $a=10;
         $hello="hola mundo";
@@ -23,6 +26,18 @@ and open the template in the editor.
         echo ' saludo $hello x $A y $a';
         echo "<br>";
         echo ' Saludo'. $hello. ' x ' .$A. ' y ' .$a ;
+        
+        $dia["lunes"]="1 clase" ;
+        $dia["martes"]="3 clase" ;
+        $dia["miercoles"]="4 clase" ;
+        $dia["jueves"]="2 clase" ;
+        $dia["viernes "]="3 clase" ;
+        
+        foreach ($dia as $value) {
+            echo $value ."<br>"; 
+            }
+            
+            
         ?>
     </body>
 </html>
